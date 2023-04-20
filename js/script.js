2e63b4a358e7910cbc18873663891d6836cab59e
Franciellen <franciellen_gc@hotmail.com>
2023-04-20 12:31:53 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 //En este array de strings, tenemos todos los colores
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple'];
+const button = document.getElementById('btn');
+button.addEventListener('click', changeColor);
 
 
+function changeColor() {
+  const randomColor = Math.floor(Math.random() * colors.length);
+  document.body.style.backgroundColor = colors[randomColor];
+}
